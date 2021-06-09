fe484cc9ba22f0eff9fcc1b0dd3e17d92d504b4d
denis-heka <44803580+denis-heka@users.noreply.github.com>
2021-06-09 19:04:45 +0100

    got it working!

---
+++ watch.js
@@ -0,0 +1,22 @@
+const esbuild = require("esbuild");
+const servor = require("servor");
+
+esbuild.build({
+  // pass any options to esbuild here...
+  entryPoints: ["src/app.jsx"],
+  outdir: "dist",
+  define: { "process.env.NODE_ENV": '"production"' },
+  watch: true,
+});
+
+async function serve(){
+  console.log("running server from: http://localhost:8080/");
+  await servor({
+    // pass any options to servor here...
+    browser:true,
+    root: "dist",
+    port: 8080,
+  });
+}
+
+serve()
